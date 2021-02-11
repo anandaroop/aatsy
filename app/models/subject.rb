a87aeb9a1cb9a1adce80c04cb7251edbcba4e6dc
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2021-02-11 18:51:35 -0500

    feat: add subject names

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,4 +2,16 @@ class Subject < ApplicationRecord
   belongs_to :parent, class_name: 'Subject', foreign_key: "parent_key"
   has_many :children, class_name: 'Subject', foreign_key: "parent_key"
   has_many :terms
+
+  def name
+    preferred_term.name
+  end
+
+  def all_names
+    terms.map(&:name)
+  end
+
+  def preferred_term
+    terms.find_by(preferred: 'P')
+  end
 end
--- app/models/term.rb
+++ app/models/term.rb
@@ -1,3 +1,9 @@
 class Term < ApplicationRecord
   belongs_to :subject
+
+  scope :preferred, -> { where(preferred: 'P') }
+
+  def name
+    term
+  end
 end
