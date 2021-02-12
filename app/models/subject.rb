e91d5228e2f61ec8b27640b1f99d1f7d864f77d1
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2021-02-11 21:55:10 -0500

    feat: eager load ancestor names

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -14,6 +14,21 @@ class Subject < ApplicationRecord
     terms.find_by(preferred: 'P')
   end
 
+  # unlike default #ancestors this eager loads associated Term
+  def ancestor_nodes
+    @ancestor_nodes ||= Subject.includes(:terms).where(terms: { preferred: 'P' }).find(ancestor_ids)
+  end
+
+  # makes use of eager-loaded Terms
+  def ancestor_names
+    @ancestor_names = ancestor_nodes.map{ |s| s.terms.first.name }
+  end
+
+  # efficient hash of ancestor ids => names
+  def ancestor_map
+    @ancestor_map = ancestor_names.zip(ancestor_ids).to_h
+  end
+
   #tmp utils
   def self.sample; offset(rand(count)).limit(1).first; end
   def self.bronze; find(300010957); end
