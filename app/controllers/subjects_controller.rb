f20854b11eb6fa130abbc6ac328e640639181d5b
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2021-02-12 21:35:21 -0500

    feat: add random switcher button

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -2,4 +2,8 @@ class SubjectsController < ApplicationController
   def show
     @subject = Subject.find(params[:id])
   end
+
+  def random
+    redirect_to Subject.sample
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :subjects
+  resources :subjects do
+    collection do
+      get 'random'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
