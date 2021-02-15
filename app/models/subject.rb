d55a5c61ccb6fd71674c7d64e59cf5f594e1729f
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2021-02-15 18:58:11 -0500

    feat: ES custom indexing, searching & autocompleting

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,5 +1,6 @@
 class Subject < ApplicationRecord
   include Elasticsearch::Model
+  index_name "aatsy_subjects"
 
   has_ancestry cache_depth: true
   has_many :terms
@@ -22,6 +23,103 @@ class Subject < ApplicationRecord
     'G' => "Guide term",
     'H' => "Hierarchy name"
   }
+
+  # ELASTICSEARCH
+
+  mappings dynamic: 'false' do
+    indexes :name, type: 'text'
+    indexes :suggest, type: 'completion'
+    indexes :facet_name, type: 'keyword'
+    indexes :record_type, type: 'keyword'
+    indexes :ancestor_ids, type: 'keyword'
+    indexes :ancestor_names, type: 'keyword'
+    indexes :scope_note, type: 'text'
+    indexes :terms, type: 'text'
+  end
+
+  def as_indexed_json(options = {})
+    {
+      name: name,
+      suggest: suggestions,
+      facet_name: facet && facet.name.split[0],
+      record_type: record_type_name,
+      ancestry: ancestry,
+      ancestor_ids: ancestor_ids,
+      ancestor_names: ancestor_names,
+      scope_note: scope_note,
+      terms: terms.map(&:term)
+    }
+  end
+
+  def suggestions
+    ([]).tap do |suggestions|
+      suggestions << { input: name, weight: 8 }
+      suggestions << { input: terms.map(&:term), weight: 3 }
+      suggestions << { input: parent.name, weight: 2 } if has_parent?
+    end
+  end
+
+  # returns db-backed instances
+  def self.query(term, options = {})
+    body = {
+      query: {
+        multi_match: {
+          query: "bronze",
+          fields: [ "name^10", "scope_note^5", "terms^3", "ancestor_names"]
+        }
+      }
+    }.merge(options) # from, size, etc
+
+    Subject.search(body)
+  end
+
+  # for perf reasons, returns search hits directly from ES index, not db-backed instances
+  def self.complete(prefix, options = {})
+      body = {
+      suggest: {
+        subjects: {
+          prefix: prefix,
+          completion: {
+            field: "suggest"
+          }
+        }
+      }
+    }.merge(options) # from, size, etc
+
+    Subject.search(body).response['suggest']['subjects'].first['options']
+  end
+
+  # Can follow the query search syntax:
+  # https://www.elastic.co/guide/en/elasticsearch/reference/5.6/query-dsl-query-string-query.html#query-string-syntax
+  #
+  # For example:
+  #
+  # Subject.console_search('"bronze age" name:"bronze age"^2 NOT record_type:C', size: 3)
+  #
+  def self.console_search(q, options = {})
+    # use search hits retrieved from es
+    results = Subject.search(q, options).results
+    results.each do |s|
+        puts "\n%s %s %s %s %s\n%s\n%s\n%s" % [
+        s.name&.bold,
+        s.facet_name&.green,
+        s.record_type&.light_blue,
+        s._score.to_s.light_magenta,
+        s._id.light_black,
+        (s.ancestor_names[2..] || []).join(" ‣ ").cyan,
+        s.scope_note,
+        s.terms.join("; ").light_black
+      ]
+    end
+    results.total
+  end
+
+  def self.recreate_index!(arel = nil)
+    __elasticsearch__.create_index! force: true
+    if arel
+      arel.import
+    end
+  end
 
   # CLASS METHODS
 
@@ -77,30 +175,6 @@ class Subject < ApplicationRecord
 
   #tmp utils
   def self.sample; offset(rand(count)).limit(1).first; end
+  def self.metal; find(300010900); end
   def self.bronze; find(300010957); end
-
-  # Can follow the query search syntax:
-  # https://www.elastic.co/guide/en/elasticsearch/reference/5.6/query-dsl-query-string-query.html#query-string-syntax
-  #
-  # For example:
-  #
-  # Subject.console_search('"bronze age" name:"bronze age"^2 NOT record_type:C')
-  #
-  # To
-  # - search for a phrase everywhere,
-  # - but boost results with the phrase in the preferred term name
-  # - filter out results which are Concepts
-  #
-  def self.console_search(q)
-    Subject.search(q).records.order(:ancestry_depth).map do |s|
-      "\n%s %s %s\n%s\n%s\n%s" % [
-        s.name.bold,
-        s.ancestors[1] && s.ancestors[1].name.split[0].green,
-        s.record_type_name.light_blue,
-        (s.ancestor_names[2..] || []).join(" ‣ ").cyan,
-        s.scope_note,
-        s.terms.map(&:term).join("; ").light_black
-      ]
-    end
-  end
 end
