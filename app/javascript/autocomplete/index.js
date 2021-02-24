c119510f108b45d136cdc6e768cf1367662f0750
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2021-02-24 09:31:01 -0500

    feat: enrich the autocomplete hit

--- app/javascript/autocomplete/index.js
+++ app/javascript/autocomplete/index.js
@@ -4,7 +4,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
   input.addEventListener('input', async (event) => {
     // fetch results
-    const qs = (new URLSearchParams({q: input.value})).toString()
+    const qs = (new URLSearchParams({ q: input.value })).toString()
     const response = await fetch(`http://localhost:3000/subjects/complete.json?${qs}`, {
       "Content-type": "application/json",
       "Accept": "application/json"
@@ -21,12 +21,27 @@ window.addEventListener('DOMContentLoaded', (event) => {
       // <li>
       const item = document.createElement("li")
       item.classList.add("autocomplete__results__hit")
+
       // <a>
       const link = document.createElement("a")
       link.innerText = hit._source.name
       link.setAttribute("href", `http://localhost:3000/subjects/${hit._id}`)
-      // add li > a to dom
       item.appendChild(link)
+
+      // other meta inside the <a>
+      const div = document.createElement("div")
+      div.classList.add("hit__meta")
+      const span1 = document.createElement("span")
+      span1.innerText = [hit._source.facet_name, hit._source.record_type].join(" ")
+      span1.classList.add("hit__type")
+      const span2 = document.createElement("span")
+      span2.innerText = hit._source.scope_note
+      span2.classList.add("hit__preview")
+      div.appendChild(span1)
+      div.appendChild(span2)
+      link.appendChild(div)
+
+      // add to dom
       results.appendChild(item)
     })
   })
